cd3587c2c5f438bd925aeea7370849e8359c109c
Rob Hudson <rhudson@orcasinc.com>
2009-02-09 16:34:50 -0800

    Updating to allow the template tag to set a context variable.

--- flatcontent/templatetags/flatcontent_tags.py
+++ flatcontent/templatetags/flatcontent_tags.py
@@ -5,25 +5,42 @@ from flatcontent.models import FlatContent
 register = template.Library()
 
 class FlatContentNode(template.Node):
-    def __init__(self, slug):
+    def __init__(self, slug, context_var=None):
         self.slug = slug
+        self.context_var = context_var
 
     def render(self, context):
-        return FlatContent.get(slug=self.slug)
+        if not self.context_var:
+            return FlatContent.get(slug=self.slug)
+        else:
+            context[self.context_var] = FlatContent.get(slug=self.slug)
+        return ''
 
 def do_flatcontent(parser, token):
     """
-    Retrieves content from the FlatContent model given a slug.
+    Retrieves content from the ``FlatContent`` model given a slug, and
+    optionally stores it in a context variable.
     
     Usage::
     
-        {% flatcontent slug %}
+        {% flatcontent [slug] %}
+    
+    Or to get the flatcontent into a variable for later use in the template or
+    with tags and filters::
+    
+        {% flatcontent [slug] as [varname] %}
     
     """
-    try:
-        tag_name, slug = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError, "The flatcontent tag requires exactly one argument"
-    return FlatContentNode(slug)
+    bits = token.split_contents()
+    len_bits = len(bits)
+    varname = None
+    if len_bits not in (2, 4):
+        raise template.TemplateSyntaxError, "The flatcontent tag requires either 1 or 3 arguments"
+    if len_bits == 2:
+        return FlatContentNode(bits[1])
+    elif len_bits == 4:
+        if bits[2] != 'as':
+            raise TemplateSyntaxError("The second argument to flatcontent tag must be 'as'")
+        return FlatContentNode(bits[1], bits[3])
 
 register.tag('flatcontent', do_flatcontent)
