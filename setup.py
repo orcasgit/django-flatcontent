c3d45a2b221cebda8a2a8f3f2a1b6c586df268d4
Rob Hudson <rhudson@orcasinc.com>
2009-04-02 14:18:51 -0700

    Adding setup.py file

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-flatcontent',
+    version=__import__('flatcontent').__version__,
+    description='Django FlatContent is intended as a flatpages-like app but for smaller chunks of content that can be edited in the Django admin.',
+    long_description=open('README.rst').read(),
+    author='Oregon Center for Applied Science',
+    author_email='support@orcasinc.com',
+    url='http://github.com/orcasgit/django-flatcontent/tree',
+    download_url='http://github.com/orcasgit/django-flatcontent/downloads',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False, # because we're including media that Django needs
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
+
