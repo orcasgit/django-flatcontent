6994f9d879190650b16f75be3b90b77822785ee7
Brad Pitcher <bradpitcher@gmail.com>
2014-05-06 09:50:52 -0700

    remove u prefix from strings
    they are all ascii anyway, the u prefix was the only thing breaking python 3.2 usage

--- flatcontent/migrations/0002_auto__add_field_flatcontent_site__del_unique_flatcontent_slug__add_uni.py
+++ flatcontent/migrations/0002_auto__add_field_flatcontent_site__del_unique_flatcontent_slug__add_uni.py
@@ -9,40 +9,40 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Removing unique constraint on 'FlatContent', fields ['slug']
-        db.delete_unique(u'flatcontent_flatcontent', ['slug'])
+        db.delete_unique('flatcontent_flatcontent', ['slug'])
 
         # Adding field 'FlatContent.site'
-        db.add_column(u'flatcontent_flatcontent', 'site',
+        db.add_column('flatcontent_flatcontent', 'site',
                       self.gf('django.db.models.fields.related.ForeignKey')(to=orm['sites.Site'], null=True, blank=True),
                       keep_default=False)
 
         # Adding unique constraint on 'FlatContent', fields ['slug', 'site']
-        db.create_unique(u'flatcontent_flatcontent', ['slug', 'site_id'])
+        db.create_unique('flatcontent_flatcontent', ['slug', 'site_id'])
 
 
     def backwards(self, orm):
         # Removing unique constraint on 'FlatContent', fields ['slug', 'site']
-        db.delete_unique(u'flatcontent_flatcontent', ['slug', 'site_id'])
+        db.delete_unique('flatcontent_flatcontent', ['slug', 'site_id'])
 
         # Deleting field 'FlatContent.site'
-        db.delete_column(u'flatcontent_flatcontent', 'site_id')
+        db.delete_column('flatcontent_flatcontent', 'site_id')
 
         # Adding unique constraint on 'FlatContent', fields ['slug']
-        db.create_unique(u'flatcontent_flatcontent', ['slug'])
+        db.create_unique('flatcontent_flatcontent', ['slug'])
 
 
     models = {
-        u'flatcontent.flatcontent': {
+        'flatcontent.flatcontent': {
             'Meta': {'unique_together': "(('slug', 'site'),)", 'object_name': 'FlatContent'},
             'content': ('django.db.models.fields.TextField', [], {}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['sites.Site']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']", 'null': 'True', 'blank': 'True'}),
             'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255'})
         },
-        u'sites.site': {
-            'Meta': {'ordering': "(u'domain',)", 'object_name': 'Site', 'db_table': "u'django_site'"},
+        'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
             'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         }
     }
