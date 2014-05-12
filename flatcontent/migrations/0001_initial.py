6032903ae5ce5718c8c18d90cea1c8cdcc1669af
Brad Pitcher <bradpitcher@gmail.com>
2014-05-11 20:51:23 -0700

    support python 3.2

--- flatcontent/migrations/0001_initial.py
+++ flatcontent/migrations/0001_initial.py
@@ -9,21 +9,21 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'FlatContent'
-        db.create_table(u'flatcontent_flatcontent', (
+        db.create_table('flatcontent_flatcontent', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=255)),
             ('content', self.gf('django.db.models.fields.TextField')()),
         ))
-        db.send_create_signal(u'flatcontent', ['FlatContent'])
+        db.send_create_signal('flatcontent', ['FlatContent'])
 
 
     def backwards(self, orm):
         # Deleting model 'FlatContent'
-        db.delete_table(u'flatcontent_flatcontent')
+        db.delete_table('flatcontent_flatcontent')
 
 
     models = {
-        u'flatcontent.flatcontent': {
+        'flatcontent.flatcontent': {
             'Meta': {'object_name': 'FlatContent'},
             'content': ('django.db.models.fields.TextField', [], {}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
